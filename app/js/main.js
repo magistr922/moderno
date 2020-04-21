c9d04c097125efff72b4635dc9801d935d5137c6
Nastya <nastya@MBP-Nastya.fritz.box>
2020-04-21 15:46:40 +0200

    added upload-page

--- app/js/main.js
+++ app/js/main.js
@@ -44,6 +44,8 @@ $(function(){
         return false;
       });
 
+      $('input[type="file"], select').styler();
+
    
     $('.menu__btn').on('click', function() {
       $('.menu__list').slideToggle();
